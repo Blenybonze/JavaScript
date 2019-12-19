e6d4da3982d620f15cfa4e41a268ce1d09aad2ce
Bleny Bonze Bernabe de Souza <blenybonze@hotmail.com>
2019-12-19 11:24:08 -0300

    Delete 1-externo.js

--- 1-externo.js
+++
@@ -1,3 +0,0 @@
-var nome = prompt("Qual seu nome?"); /*Armazenando uma variável na memória da maquina do usuário(var=variável / nome=nome da variável criada / prompt=o que aparecera na tela para o usuário)*/
-
-alert("Bem vindo, " + nome); /*Criando um alert que utiliza a variável criada (Texto sempre dentro de "duas aspas" e fora o sinal de + juntamente do nome da variável)*/
