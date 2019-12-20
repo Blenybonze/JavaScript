dab7860e40a52949c4510dff64a204c882def19a
Bleny Bonze Bernabe de Souza <blenybonze@hotmail.com>
2019-12-20 13:54:40 -0300

    Media aritim.
    Adicionando pasta complementar para a lógica de programação

---
+++ 00 - Lógica de programação/02 - Média Aritimética/0-media.js
@@ -0,0 +1,12 @@
+    var n1 = prompt("Digite um número"); //solicita um número
+    n1 = parseFloat(n1);
+
+    var n2 = prompt("Digite outro número");//solicita outro número
+    n2 = parseFloat(n2);
+
+    var media = ((n1+n2)/2);//Crie uma variável que terá o resultado
+    alert("A média é: "+ media); //Aplique a variável no alert
+
+
+    //OOOOUUUUU //Outra forma de resolver a média sem criar variável
+    //alert("A média é: "+((n1+n2)/2)); 
