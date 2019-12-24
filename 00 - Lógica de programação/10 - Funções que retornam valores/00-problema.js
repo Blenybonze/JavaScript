132775e958c44a2a64143f26295dc267f21a3c89
Bleny Bonze Bernabe de Souza <blenybonze@hotmail.com>
2019-12-24 11:21:46 -0300

    Funções que retornam valores

---
+++ 00 - Lógica de programação/10 - Funções que retornam valores/00-problema.js
@@ -0,0 +1,4 @@
+/*
+Pedir para o usuário digitar dois números que representam os lados de um retângulo.
+Mostrar na tela a área total de duas formas distintas: Num alert ou na div #output(dependendo do botão clicado). Esse código não precisa ser executado quando a página é aberta.
+/*
