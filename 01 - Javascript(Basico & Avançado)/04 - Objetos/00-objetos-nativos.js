f1d963ff1129107c0adf6ed18d7b60999a4967a2
Bleny Bonze Bernabe de Souza <blenybonze@hotmail.com>
2019-12-31 13:42:26 -0300

    Um pouco sobre sintaxe

---
+++ 01 - Javascript(Basico & Avançado)/04 - Objetos/00-objetos-nativos.js
@@ -0,0 +1,11 @@
+
+    var minhaStr = "Bleny";
+
+    //para ver quantos caracteres existem na string basta digitar:
+    minhaStr.length
+
+    var teste = new String('minha string de teste');
+
+
+    //Neste caso o .length funcionará para saber quantos conjuntos de string existem dentro do array
+    var arr = new Array('eu', 'não', 'sei');
---
+++ 01 - Javascript(Basico & Avançado)/04 - Objetos/01-sintaxe.js
@@ -0,0 +1,7 @@
+
+    //Sintaxe Formal
+    var obj = new Object()
+
+
+    //Sintaxe Literal
+    obj = {'title': 'titulo,', 'texto': 'loren'};
