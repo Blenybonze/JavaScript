af7ad9b3e165081c08091ae3d1a81b89dd7ded0d
Bleny Bonze Bernabe de Souza <blenybonze@hotmail.com>
2020-01-09 11:20:27 -0300

    Novo Desafio

---
+++ 01 - Javascript(Basico & Avançado)/05 - Eventos/12-problema5.js
@@ -0,0 +1,6 @@
+/*
+Fazer uma pequena lista de tarefas onde o usuário pode acrescentar um novo 
+item (li) na lista (ul) através de um input e um botão. Esta lista deve 
+estar prenchida previamente com três itens quaiser.
+Acrescentar eventos de click em cada li para marcar como feito.
+*/
