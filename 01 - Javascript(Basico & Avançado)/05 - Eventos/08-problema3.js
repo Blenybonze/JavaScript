056f58cef58ef94cb0b0c79b054c88db06f8f386
Bleny Bonze Bernabe de Souza <blenybonze@hotmail.com>
2020-01-09 11:16:33 -0300

    Novo desafio

---
+++ 01 - Javascript(Basico & Avançado)/05 - Eventos/08-problema3.js
@@ -0,0 +1,6 @@
+/*
+Permitir o envio de um formulário somente após o usuário aceitar os termos
+do contrato(Checkbox).
+Para este exemplo, vamos habilitar ou desabilita o botão dependndo do checkbox.
+
+*/
