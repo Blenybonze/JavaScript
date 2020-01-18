69844eeb252e873a3fb195267be9d877ff0712ee
Bleny Bonze Bernabe de Souza <blenybonze@hotmail.com>
2020-01-18 13:33:29 -0300

    Escrevendo funções de forma arrow function

---
+++ 01 - Javascript(Basico & Avançado)/09 - Métodos/09-arrow-functions.js
@@ -0,0 +1,9 @@
+/*a principio uma versão mais simplificada de escrever funções
+usarei como exemplo um trecho de código avulso
+Exemplo escrito como antes:*/
+var exemplo = exemplo2.some(function(nota){
+    return nota === 0;
+});
+
+/*Exemplo escrito novamente:*/
+var exemplo = exemplo2.some( nota => nota === 0);
