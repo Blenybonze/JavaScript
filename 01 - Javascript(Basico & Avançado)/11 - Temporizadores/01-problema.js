57c5aef8be9693e491e6e0f02865ce5b1ccfa675
Bleny Bonze Bernabe de Souza <blenybonze@hotmail.com>
2020-01-22 18:39:47 -0300

    Primeiro problema

---
+++ 01 - Javascript(Basico & Avançado)/11 - Temporizadores/01-problema.js
@@ -0,0 +1,6 @@
+/*
+O cliente pediu para fazer um pop-up bem irritante que PULA na interface do 
+usuário depois de 5 segundos. Este mesmo pop-up deve ser fechado 
+automaticmaente após 5 segundos(mesmo que o usuário ainda estava-o lendo.
+Lembre-se, a ideia é que seja extremamente irritante). 
+*/
