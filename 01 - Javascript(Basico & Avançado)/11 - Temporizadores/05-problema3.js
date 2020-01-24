a0e155d208fd5b85fb6e12cc92a8ca53a6f1942e
Bleny Bonze Bernabe de Souza <blenybonze@hotmail.com>
2020-01-23 22:13:24 -0300

    Problema 3

---
+++ 01 - Javascript(Basico & Avançado)/11 - Temporizadores/05-problema3.js
@@ -0,0 +1,7 @@
+/*
+Mostrar cronometro.
+Faça uma interface contendo um input text e três botões.
+Ao clicar no primeiro deve iniciar um cronômetro.
+O segundo deve pausar ou continuar.
+O terceiro deve resetar o cronometro.
+*/
