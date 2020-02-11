9302ba18928866a661b4630d67a363a212c3bf65
Bleny Bonze Bernabe de Souza <blenybonze@hotmail.com>
2020-02-11 17:38:33 -0300

    Convertendo numeros e strings

---
+++ 00 - Lógica de programação/00 - Introdução/ex003.js
@@ -0,0 +1,9 @@
+//se não fizer a conversão para numero, o prompt automaticamente colhe
+//o dado como string
+var n1 = Number(window.prompt('digite um número'));
+var n2 = Number(window.prompt('digite outro numero'));
+var soma = n1 + n2;
+alert(`A soma de ${n1} + ${n2} é igual a ${soma}`);
+//(number + number) Para adição
+//(string + string) Para concatenação
+// Para converter de número par string fica string(variável)
