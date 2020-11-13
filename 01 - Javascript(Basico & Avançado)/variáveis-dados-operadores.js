7946788cb4df217c19dab612ca746180a682d828
bleny bonze <blenybonze@hotmail.com>
2020-11-13 19:09:59 -0300

    Criando função construtora para pôr em prática a criação de objetos e acesso de métodos

---
+++ 01 - Javascript(Basico & Avançado)/variáveis-dados-operadores.js
@@ -0,0 +1,22 @@
+var DevFront = new Pessoa("adriano","souza")
+
+// Objeto com função construtora
+function Pessoa(nome, sobrenome) {
+  this.nome = nome;
+  this.sobrenome = sobrenome;
+  this.falar = function ( ) {
+      return ("Meu nome é " + this.nome + " " + this.sobrenome);
+  }
+ }
+ 
+ 
+ // Criando um objeto a partir de uma função construtora
+ var funcionario = new Pessoa("Maria", "Peixoto");
+ 
+ // Acessando a propriedade nome e o método falar
+ funcionario.nome;
+ funcionario.falar();
+
+ // chamando a função
+ console.log(funcionario.falar());
+ 
