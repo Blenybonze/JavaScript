bbfdb4a9ca39effcb1ebcca40d8b0b3fc4bfa750
bleny bonze <blenybonze@hotmail.com>
2020-11-17 11:37:56 -0300

    Checagem de usuário e senha por meio de estrutura condicional

---
+++ js.js
@@ -0,0 +1,11 @@
+let usuario = "Manoelly"
+let senha = '5648'
+
+if(usuario === "Manoelly" && senha == "5648"){
+  mensagem = "Autenticado"
+}
+else{
+  mensagem = "Usuário e/ou senha inválido"
+}
+
+console.log(mensagem)
