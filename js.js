30440c8427a873a1c63fe71481cd33cf96fd8130
bleny bonze <blenybonze@hotmail.com>
2020-11-24 18:57:21 -0300

    Dessa vez fui mais longe e somei os valores dentro de {objetos} contidos em um [Array]

--- js.js
+++ js.js
@@ -1,17 +1,25 @@
-// Verificando valores dentro de um Array
+//Somando os valores dentro de um Array[]
 
-const cores= [
-  "verde",
-  "amarelo",
-  "azul",
-  "branco",
-  "roxo",
-  "rosa"
+const produtos = [
+  {descricao:"Resma Oficio", valor:11.50, quantidade:10},
+  {descricao:"lapis preto", valor:0.50, quantidade:3},
+  {descricao:"Paleta Plástica", valor:1.20, quantidade:4},
+  {descricao:"Caneta", valor:2, quantidade:5}
 ];
 
+
+let total = 0;
+let totalProduto=0;
 let index = 0;
 
-while(index < cores.length){
-  console.log('A cor é '+cores[index]);
+while(index<produtos.length){
+  const{descricao,valor,quantidade} = produtos[index];
+
+  totalProduto = valor * quantidade;
+  total += totalProduto;
   index++;
+  
+  console.log("Total de "+descricao+" é "+totalProduto);
 }
+
+console.log("O Total geral é: R$"+total.toFixed(2));
