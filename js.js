bde79c2349659a3ad83f63121b68182755099d22
bleny bonze <blenybonze@hotmail.com>
2020-11-25 20:38:32 -0300

    MEsmo exemplo de ontem feito com o while, porém dessa vez feito com for in

--- js.js
+++ js.js
@@ -1,9 +1,20 @@
 // exemplos com for in e for off
 
-//imprimindo as cores dentro de um array
+const produtos = [
+  { descricao: 'Resma Ofício', valor: 11.50, quantidade: 10 },
+  { descricao: 'Lapis preto', valor: 0.50, quantidade: 3 },
+  { descricao: 'Transferidor plastico', valor: 1.20, quantidade: 4 }
+]
 
-let cores = ['verde','amarelo','roxo'];
+let totalGeral = 0;
+let totalProduto = 0;
 
-for(const index in cores){
-  console.log(index,cores[index]);
+for(const index in produtos){
+  const{valor,quantidade,descricao} = produtos[index];
+  totalProduto = valor * quantidade;
+  totalGeral += totalProduto;
+  console.log('produto '+descricao+' tem o total em Reais de R$'+totalProduto.toFixed(2));
 }
+
+console.log('O total Geral de todos os produtos estocados é de: R$'+totalGeral.toFixed(2))
+
