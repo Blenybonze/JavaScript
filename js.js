e50ca02114afc3b40e9ad0daf8f3a8448852e0cb
bleny bonze <blenybonze@hotmail.com>
2020-11-26 18:23:31 -0300

    Imprimindo as datas com parâmetros diferentes

--- js.js
+++ js.js
@@ -1,12 +1,16 @@
-const produtos = [
-  {id: 20, tipo: "Filme"},
-  {id: 10, tipo: "Jogo"},
-  {id: 8, tipo: "Música"},
-];
+// Aprendendo a usar o new Date()
 
-for(const produto of produtos)
- {
-  if ( produto.id === 10 || produto.id === 8 ) {
-    console.log(produto.tipo);
-  }
-}
+//Criando sem parâmetro
+let data = new Date();
+
+//Com parâmetro
+let exemplo1 = new Date("2020-11-25");
+let exemplo2 = new Date("2020-11-25 18:14:00");
+
+//Simplificando o parâmetro colocando numeros com virgula
+let exemplo3 = new Date(2020,11,24,17,15,00)
+
+console.log(data)
+console.log(exemplo1)
+console.log(exemplo2)
+console.log(exemplo3)
