bfffc68f61539998296354333bb341afed1de167
bleny bonze <blenybonze@hotmail.com>
2021-01-16 10:55:23 -0300

    Finalizado o layout base das telas, iniciando a programação em JS

---
+++ projetoAnotacoes/script.js
@@ -0,0 +1,23 @@
+const reset =  document.querySelector('.reset');
+const data = document.querySelectorById('data');
+const lista = document.querySelectorById('lista');
+const addTarefa = document.querySelectorById('addTarefa');
+
+
+
+
+function adicionarTarefa(){
+  if(addTarefa.value != ''){
+    
+  }
+}
+function resetarTudo(){
+  alert('reset');
+}
+ 
+function removerTarefa(){
+  alert('remover');
+}
+function concluirTarefa(){
+  alert('concluir');
+}
