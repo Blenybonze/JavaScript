350b20882a149204004b34f30d72f89f20733f30
bleny bonze <blenybonze@hotmail.com>
2021-01-16 12:51:48 -0300

    checkpoint no desenvolvimento da lista de tarefas, criada a primeira função que adiciona novas li's ao projeto

--- projetoAnotacoes/script.js
+++ projetoAnotacoes/script.js
@@ -1,16 +1,48 @@
-const reset =  document.querySelector('.reset');
-const data = document.querySelectorById('data');
-const lista = document.querySelectorById('lista');
-const addTarefa = document.querySelectorById('addTarefa');
+const reset = document.querySelector('.reset');
+const data = document.getElementById('data');
+const lista = document.getElementById('lista');
+const addTarefa = document.getElementById('addTarefa');
+const item = document.querySelector('.item');
+
+
+
+
+let LIST = [];
+let id = 0;
+
+function criarItem(tarefa){
+  tarefa = addTarefa.value;
+  
+  
+  if(tarefa != ''){
+  
+    LIST.push ({
+      nome:tarefa,
+      id:0
+    });
+
+  const texto = `<li class="item">
+                  <i class="far fa-circle eft-btn" onclick="concluirTarefa()" aria-hidden="true"></i>
+                  <p class="textoItem">${tarefa}</p>
+                  <i class="fas fa-trash-alt eft-btn" onclick="removerTarefa()" aria-hidden="true"></i>
+                </li>`
+  const posicao = 'beforeend'
+  lista.insertAdjacentHTML(posicao,texto);
+  
+  id++;
+}
+  else{
+    addTarefa.value = '';
+}}
 
 
 
 
 function adicionarTarefa(){
-  if(addTarefa.value != ''){
-    
-  }
+  criarItem(addTarefa.value);
+  addTarefa.value = '';
 }
+
 function resetarTudo(){
   alert('reset');
 }
