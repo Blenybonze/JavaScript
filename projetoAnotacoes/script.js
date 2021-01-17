9d60bb92e8898bdd7527be6f31fa5c57d419a474
bleny bonze <blenybonze@hotmail.com>
2021-01-17 15:14:00 -0300

    Projeto finalizado até onde eu consegui ir, farei mais ajustes e melhorias assim que obtiver mais conhecimento tecnico sobre JS

--- projetoAnotacoes/script.js
+++ projetoAnotacoes/script.js
@@ -1,55 +1,62 @@
+//Variáveis de seleção
 const reset = document.querySelector('.reset');
 const data = document.getElementById('data');
 const lista = document.getElementById('lista');
 const addTarefa = document.getElementById('addTarefa');
-const item = document.querySelector('.item');
+const item = document.getElementsByClassName('item');
+
+//DATA
+let hoje = new Date; 
+data.innerHTML = hoje.getDate()+'/'+hoje.getMonth()+1+'/'+hoje.getFullYear();
 
 
-
-
+//variáveis para controle de id e lista
 let LIST = [];
 let id = 0;
 
+
+//função que cria os intens dentro da ul
 function criarItem(tarefa){
   tarefa = addTarefa.value;
-  
-  
-  if(tarefa != ''){
+  if(tarefa != '' && LIST.length<=9){
   
     LIST.push ({
       nome:tarefa,
-      id:0
+      id:LIST.length
     });
 
   const texto = `<li class="item">
                   <i class="far fa-circle eft-btn" onclick="concluirTarefa()" aria-hidden="true"></i>
                   <p class="textoItem">${tarefa}</p>
-                  <i class="fas fa-trash-alt eft-btn" onclick="removerTarefa()" aria-hidden="true"></i>
+                  <i class="fas fa-trash-alt eft-btn btn-del" onclick="removerTarefa()" aria-hidden="true"></i>
                 </li>`
   const posicao = 'beforeend'
   lista.insertAdjacentHTML(posicao,texto);
   
   id++;
+  }
 }
-  else{
-    addTarefa.value = '';
-}}
 
 
-
-
+//botão que envia o valor para as li's e limpa o campo de texto
 function adicionarTarefa(){
   criarItem(addTarefa.value);
   addTarefa.value = '';
 }
 
+
+//Resetar Lista
 function resetarTudo(){
-  alert('reset');
+  document.location.reload(true);
 }
- 
+
+
+//Ambas as funções encontrei diversas dificuldades em acompanhar as li's para deletar e concluir'las
+//Peço desculpas pela inconclusão
 function removerTarefa(){
-  alert('remover');
+  alert('Remover');
 }
 function concluirTarefa(){
-  alert('concluir');
+  alert('Concluir')
 }
+
